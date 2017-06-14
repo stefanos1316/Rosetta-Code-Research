4261574a7e099b99b063977e243cfac6c83379f4
stefanos1316 <stefanos1316@gmail.com>
2017-06-15 00:37:21 +0300

    Added new file script.findCommonTasks

--- Scripts/Task/100-doors/java/100-doors-1.java
+++
@@ -1,1 +0,0 @@
-N.println(IntStream.rangeClosed(1, 100).filter(i -> Math.pow((int) Math.sqrt(i), 2) == i).boxed().join(", ", "Open Doors: ", ""));
--- Scripts/Task/100-doors/perl/100-doors-4.pl
+++
@@ -1,1 +0,0 @@
-print "Door $_ is ", qw"closed open"[int sqrt == sqrt], "\n" for 1..100;
