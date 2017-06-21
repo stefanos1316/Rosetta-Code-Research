aa257f79d3da8cc7ff312b5bcf7dc24080db4b46
stefanos1316 <stefanos1316@gmail.com>
2017-06-21 14:20:38 +0300

    Successfuly installed and added Swift

--- Scripts/newTask/array-concatenation/swift/array-concatenation.swift
+++ Scripts/newTask/array-concatenation/swift/array-concatenation.swift
@@ -1,3 +1,9 @@
-let array1 = [1,2,3]
-let array2 = [4,5,6]
-let array3 = array1 + array2
+let array1 = [1,2,3,4,5]
+let array2 = [6,7,8,9,10]
+
+for i in 1...1000000 {
+	let array3 = array1 + array2
+	for elements in array3 {
+		print(elements)
+	}
+}
