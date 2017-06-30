0928a5ba5e812862642ee44d09c061581465ba93
stefanos1316 <stefanos1316@gmail.com>
2017-06-30 12:44:23 +0300

    Added swift implementation for Bubble Sort

--- Scripts/newTask/sorting-algorithms-bubble-sort/swift/sorting-algorithms-bubble-sort.swift
+++ Scripts/newTask/sorting-algorithms-bubble-sort/swift/sorting-algorithms-bubble-sort.swift
@@ -1,13 +1,23 @@
-var inputArr = [12,56,32,23,67,87,45,23,10,11]
-func swapNumbers(index1 :Int,index2: Int) {
-let temp = inputArr[index1]
-inputArr[index1] = inputArr[index2]
-inputArr[index2] = temp
-}
-for var index: Int = inputArr.count–1; index > 1; –index {
-for var jIndex: Int = 0; jIndex < index; ++jIndex {
-if inputArr[jIndex] > inputArr[jIndex + 1] {
-swapNumbers(jIndex, jIndex+1)
-}
-}
+
+func bubbleSort(_ array: [Int]) -> [Int] {
+        var sorted = array
+        var swappedValue = Bool()
+        
+        repeat {
+            swappedValue = false
+
+            for (index, _) in sorted.enumerated() {
+                if sorted.count > index+1 && 
+sorted[index] > sorted[index+1] {
+                    swap(&sorted[index], &sorted[index+1])
+                    swappedValue = true
+                }
+            }
+        } while swappedValue
+        return sorted
+    }
+
+for i in 1...1000000 {
+let array = [16,93,-99,95,-96,-24,-53,-71,96,-66,-21,72,-12,-32,-96,62,-42,-50,49,53,-65,52,-25,-69,88,-43,60,66,-94,-69,53,-71,-17,-58,-30,32,-16,-94,-42,-86,59,-53,94,97,-12,15,65,-35,-12,-82,-82,48,-48,66,-42,-63,33,-49,41,-85,94,66,-60,60,-65,-73,-50,-9,-48,-3,15,-77,81,95,-93,83,-21,34,-78,-61,-22,-58,65,78,41,-7,-59,20,88,-38,-15,-69,42,97,-35,61,8,2,-22,-85]
+let sortedArray = bubbleSort(array)
 }
