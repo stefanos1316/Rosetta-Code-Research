ce62dd8a076c999c3a569d20554144a705ae4185
Stefanos Georgiou <stefanos1316@gmail.com>
2017-08-27 16:07:33 +0300

    Update array-concatenation-1.go

--- Scripts/newTask/array-concatenation/go/array-concatenation-1.go
+++ Scripts/newTask/array-concatenation/go/array-concatenation-1.go
@@ -5,7 +5,7 @@ func main() {
     // Example 1:  Idiomatic in Go is use of the append function.
     // Elements must be of identical type.
     
-	for i := 0; i < 10000000; i++ {
+	for i := 0; i < 1000000; i++ {
    		a := []int{1, 2, 3, 4 ,5}
     		b := []int{6, 7, 8, 9, 10} // these are technically slices, not arrays
 		c := append(a, b...)
